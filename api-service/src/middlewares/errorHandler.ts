f2130a0ef7bb4f337344e33074bab8f5a98db66a
isai-mykyta <mykytaisai@gmail.com>
2025-01-18 18:55:31 +0200

    join it if you can't fight it -\_(**)_/-

--- api-service/src/middlewares/errorHandler.ts
+++ api-service/src/middlewares/errorHandler.ts
@@ -1,7 +1,8 @@
 import { ErrorRequestHandler, NextFunction, Request, Response } from "express";
+import { ValidateError } from "tsoa";
 
 import { ApplicationError } from "../common";
-import { HttpStatusCode } from "../types";
+import { CustomErrorType, HttpStatusCode } from "../types";
 
 export const errorHandler: ErrorRequestHandler = (err: ApplicationError | Error, req: Request, res: Response, next: NextFunction): void => {
   console.error("Error:", err);
@@ -9,6 +10,17 @@ export const errorHandler: ErrorRequestHandler = (err: ApplicationError | Error,
   if (err instanceof ApplicationError) {
     res.status(err.statusCode).json(err.getErrorData()); 
     return; 
+  }
+
+  // support for tsoa validation errors
+  if (err instanceof ValidateError) {
+    const applicationError = new ApplicationError({
+      message: err.message,
+      type: CustomErrorType.VALIDATION_ERROR,
+      statusCode: HttpStatusCode.BAD_REQUEST,
+      details: err.fields,
+    });
+    res.status(applicationError.statusCode).json(applicationError.getErrorData()); 
   }
 
   res.status(HttpStatusCode.INTERNAL_ERROR).json({ message: "Internal Server Error"});
